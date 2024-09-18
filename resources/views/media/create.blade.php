ff316a14ae23fb6d23e0f06202193c0274cda0cc
Ahmed Shabbir <ahmed.shabbir@manafatech.com>
2024-09-19 02:01:18 +0500

    add media files

---
+++ resources/views/media/create.blade.php
@@ -0,0 +1,17 @@
+@extends('Layout.master')
+
+@section('title')
+ Create Logo
+@endsection
+
+@section('content')
+
+<div class="page-content">
+<div class="page-header"><h1>Logos</h1></div>
+
+@include('media.form')
+
+</div>
+
+@endsection
+
---
+++ resources/views/media/lists.blade.php
@@ -0,0 +1,191 @@
+@extends('Layout.master')
+
+@section('title')
+Media Gallery
+@endsection
+
+@section('content')
+
+<div class="page-content">
+
+<div class="page-header" style="min-height:40px;">
+    <div class="" style="float: left;">
+        <h1>Media Gallery</h1>
+    </div>
+    <div class="" style="float: right;">
+        <a href="{{ route('media.create') }}" class="btn btn-xs btn-light bigger"><i class="ace-icon fa fa-floppy-o"></i> Media Gallery </a> 
+    </div>
+</div>
+
+
+
+
+<div class="row ">
+    <div class="col-12 col-lg-12" style="margin-top:20px;">
+
+        <div class="card radius-10 border-top border-0 border-4 border-danger">
+
+
+            <div class="card-body">
+
+                @include('Layout.alerts')
+
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered yajratable" id="yajra-table" style="width:100%"></table>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+<!--end row-->
+
+</div>
+
+<!--- delete confirmation modal --->
+<div class="modal" tabindex="-1" role="dialog" id="deletePopup">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+
+            <form action="#" method="post" id="delete_form">
+                <input type="hidden" name="_method" value="delete" />
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title">confirmation</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Do you really want to delete ?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Confirm</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+<!--- delete confirmation modal end --->
+@endsection
+
+
+
+
+@section('script')
+<script>
+    var table;
+    var selectID;
+
+
+    function doChangeStatus(objID){
+        var changeURL = "{{ route('media.status','') }}";
+        $.ajax({
+            dataType: 'json',
+            type:'get',
+            url : changeURL + '/' + objID,
+            success:function(res){
+                console.log('yes');
+                if(res.status){
+                    table.ajax.reload();
+                    $('#statusPopup').modal('hide');
+                } 
+            }
+        });
+    }
+
+    function deleteConfirmation(id) {
+        selectID = id;
+        $.confirm({
+            title: 'Confirmation',
+            content: 'Do you really want to change status ?',
+            buttons: {
+                yes: {
+                    text: 'Yes',
+                    action: function() {
+                        var changeURL = "{{ route('media.delete','') }}";
+                        window.location = changeURL + '/' + selectID;
+                    }
+                },
+                no: {
+                    text: 'No', // With spaces and symbols
+                    action: function() {
+
+                    }
+                }
+            }
+        });
+    }
+
+
+    function changeStatus(id) {
+        selectID = id;
+        $.confirm({
+            title: 'Confirmation',
+            content: 'Do you really want to change status ?',
+            buttons: {
+                yes: {
+                    text: 'Yes',
+                    action: function() {
+                        doChangeStatus(id)
+                    }
+                },
+                no: {
+                    text: 'No', // With spaces and symbols
+                    action: function() {
+
+                    }
+                }
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        table = $('#yajra-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('media.list') }}",
+            columns: [
+                {
+                    data: 'id',
+                    name: 'id',
+                    title: 'ID',
+                   
+                },
+                {
+                    data: 'image_thumb',
+                    name: 'image_thumb',
+                    title: 'Image'
+                },
+                {
+                    data: 'post_title',
+                    name: 'post_title',
+                    title: 'Title'
+                },
+                {
+                    data: 'user_name',
+                    name: 'user_name',
+                    title: 'Author'
+                },
+                {
+                    data: 'status_label',
+                    name: 'status',
+                    title: 'Status'
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    title: 'Action'
+                }
+            ],
+            // order: [[1, 'asc']]
+        });
+    });
+</script>
+@endsection
