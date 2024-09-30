efab0855f327334253efbe75d619490caa873b7a
Ahmed Shabbir <ahmed.shabbir@manafatech.com>
2024-09-30 14:43:18 +0500

    add home file

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <style>
+        @font-face {
+            font-family: 'Sk-Modernist';
+            src: url('fonts/Sk-Modernist-Regular.otf');
+            
+        }
+        @font-face {
+            font-family: 'Sk-Modernist';
+            src: url('fonts/Sk-Modernist-Bold.otf');
+            font-weight: bold;
+        }
+        body {
+            font-family: 'Sk-Modernist';
+            margin: 0;
+            padding: 0;
+            text-align: center;
+            background-color: #f4f4f4;
+        }
+        .container {
+            padding: 20px;
+        }
+        img {
+            display: block;
+            margin: 50px auto 50px auto;
+            max-width: 100%;
+            height: auto;
+        }
+        .p {
+            margin: 20px 0;
+            color: #000000;
+        }
+        h1 {
+            font-size: 2.5em;
+            font-weight: bold;
+        }
+        h2 {
+            font-size: 2em;
+            font-weight: normal;
+        }
+        h3 {
+            font-size: 1.5em;
+            font-weight: lighter;
+        }
+        h4 {
+            font-size: 1.2em;
+            font-weight: lighter;
+        }
+
+
+        .small {
+    font-size: 1.2em;
+}
+
+.medium {
+    font-size: 1.5em;
+}
+
+.large {
+    font-size: 2em;
+}
+
+        .button {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 20px 100px;
+            font-size: 25px;
+            color: #000;
+            background-color: transparent;
+            border: 1px solid #000;
+            border-radius: 35px;
+            text-decoration: none;
+            cursor: pointer;
+            transition: background-color 0.3s ease, color 0.3s ease;
+        }
+        .button:hover {
+            background-color: #000;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img src="images/top_logo.png" alt="top logo">
+        <p>
+            <span class="small">Prepare for the future of online logo designing.</span><br>
+            <span class="large">Next generation of <b>LOGO</b> making — launching soon!</span>
+        </p>
+        <a href="mailto:heymakelogo@gmail.com" class="button">Say hi.</a>
+    </div>
+</body>
+</html>
